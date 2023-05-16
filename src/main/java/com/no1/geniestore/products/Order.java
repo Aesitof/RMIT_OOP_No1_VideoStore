6866ea995dd1ee4954dc8584931577171faa4e9f
Nhat Anh <s3924763@rmit.edu.vn>
2023-05-16 12:21:42 +0700

    Order + OrderDetail

--- src/main/java/com/no1/geniestore/products/Order.java
+++ src/main/java/com/no1/geniestore/products/Order.java
@@ -2,58 +2,87 @@ package com.no1.geniestore.products;
 
 import com.no1.geniestore.constants.LoanType;
 
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashSet;
+import java.util.HashMap;
 
-public class Order implements OrderDetails{
-    private HashSet<Item> orderedItems = new HashSet<Item>();
-    private Date returnDate;
-    private Date loanDate;
-    private final static Integer dayLoan = 2;
-    private final static Integer weekLoan = 7;
+public class Order {
+    private final static long weekLoan = 86400000 * 7;
+    private final static long dayLoan = 86400000 * 2;
+    private String orderID;
+    HashMap<Item, OrderDetails> order = new HashMap<>();
 
-    public Order(HashSet<Item> orderedItems, Date returnDate, Date loanDate) {
-        this.orderedItems = orderedItems;
-        this.returnDate = returnDate;
-        this.loanDate = loanDate;
+    public Order(String orderID) {
+        this.orderID = orderID;
     }
+    public void addItem(Item item, Date loanDate, int amount) {
+        long currentTimeMillis = System.currentTimeMillis();
 
-    public void addOrderItem(Item item){
-
-        orderedItems.add(item);
-
-    }
-
-    public Order(HashSet<Item> orderedItems, Date returnDate) {
-        this.orderedItems = orderedItems;
-        this.returnDate = returnDate;
-        this.loanDate = new Date();
-    }
-
-    public Date getReturnDate() {
-        return returnDate;
-    }
-
-    public void setReturnDate(Date returnDate) {
-        this.returnDate = returnDate;
-    }
-
-    public Date getLoanDate() {
-        return loanDate;
-    }
-
-    public void setLoanDate(Date loanDate) {
-        this.loanDate = loanDate;
-    }
-
-    @Override
-    public void durationCalculate() {
-        for(Item orderedItem : orderedItems) {
-            if (orderedItem.getLoanType().equals(LoanType.TWO_DAY_LOAN)) {
-
-            }
-
+        OrderDetails orderDetail = new OrderDetails();
+        orderDetail.setLoanDate(loanDate);
+        orderDetail.setAmount(amount);
+        long returnDate;
+        if (item.getLoanType().equals(LoanType.TWO_DAY_LOAN)) {
+            returnDate = orderDetail.getLoanDate().getTime() + dayLoan;
+        } else {
+            returnDate = orderDetail.getLoanDate().getTime() + weekLoan;
         }
+        orderDetail.setReturnDate(new Date(returnDate));//chuyển về lại Date
+
+        order.put(item, orderDetail);
+    }
+    public void removeItem(Item item) {
+        order.remove(item);
     }
 }
+
+//public class Order implements OrderDetails{
+//    private HashSet<Item> orderedItems = new HashSet<Item>();
+//    private Date returnDate;
+//    private Date loanDate;
+//    private final static Integer dayLoan = 2;
+//    private final static Integer weekLoan = 7;
+//
+//    public Order(HashSet<Item> orderedItems, Date returnDate, Date loanDate) {
+//        this.orderedItems = orderedItems;
+//        this.returnDate = returnDate;
+//        this.loanDate = loanDate;
+//    }
+//
+//    public void addOrderItem(Item item){
+//
+//        orderedItems.add(item);
+//
+//    }
+//
+//    public Order(HashSet<Item> orderedItems, Date returnDate) {
+//        this.orderedItems = orderedItems;
+//        this.returnDate = returnDate;
+//        this.loanDate = new Date();
+//    }
+//
+//    public Date getReturnDate() {
+//        return returnDate;
+//    }
+//
+//    public void setReturnDate(Date returnDate) {
+//        this.returnDate = returnDate;
+//    }
+//
+//    public Date getLoanDate() {
+//        return loanDate;
+//    }
+//
+//    public void setLoanDate(Date loanDate) {
+//        this.loanDate = loanDate;
+//    }
+//
+//    @Override
+//    public void durationCalculate() {
+//        for(Item orderedItem : orderedItems) {
+//            if (orderedItem.getLoanType().equals(LoanType.TWO_DAY_LOAN)) {
+//
+//            }
+//
+//        }
+//    }
+//}
--- src/main/java/com/no1/geniestore/products/OrderDetails.java
+++ src/main/java/com/no1/geniestore/products/OrderDetails.java
@@ -1,5 +1,48 @@
 package com.no1.geniestore.products;
 
-public interface OrderDetails {
-    public void durationCalculate();
+import java.util.Date;
+
+public class OrderDetails {
+    private Date returnDate;
+    private Date loanDate;
+    private int amount;
+
+    public OrderDetails(Date returnDate, Date loanDate, int amount) {
+        this.returnDate = returnDate;
+        this.loanDate = loanDate;
+        this.amount = amount;
+    }
+    public OrderDetails() {
+        this.returnDate = new Date(System.currentTimeMillis());
+        this.loanDate = new Date(System.currentTimeMillis());
+        this.amount = 0;
+    }
+
+    public Date getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(Date returnDate) {
+        this.returnDate = returnDate;
+    }
+
+    public Date getLoanDate() {
+        return loanDate;
+    }
+
+    public void setLoanDate(Date loanDate) {
+        this.loanDate = loanDate;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
 }
+
+//public interface OrderDetails {
+//    public void durationCalculate();
+//}
